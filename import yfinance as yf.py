e2528457f65c42b4e447d864d828fe561f17a2a8
Reha Demircan <153535205+rdmrcn@users.noreply.github.com>
2025-09-01 11:35:32 +0300

    Update import yfinance as yf.py

--- import yfinance as yf.py
+++ import yfinance as yf.py
@@ -1,9 +1,14 @@
 import yfinance as yf
 
+##The period is currently set to "1d", which fetches 1 day of trading data.
+
+Ticker symbols must be valid (e.g., AAPL for Apple, TSLA for Tesla, MSFT for Microsoft).
+
 STK = input("Enter share name:")
 
 data = yf.Ticker(STK).history(period="id")
 
 last_market_price = data['close'].iloc[-1]
+
 
 print("Last Market Price: ", last_market_price)
